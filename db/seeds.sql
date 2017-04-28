3bb1ef3792aa2fa72fb18fd7a54a47f43000eddc
Dacia Yang <yang.dacia@gmail.com>
2017-04-28 12:57:30 -0500

    orm complete

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burger (name) VALUES ('Texas');
+INSERT INTO burger (name) VALUES ('Torchy');
+INSERT INTO burger (name) VALUES ('Tornado Burger');
+INSERT INTO burger (name) VALUES ('Bacon Heaven');
+INSERT INTO burger (name, eaten) VALUES ('El Mundo Burger', true);
+INSERT INTO burger (name) VALUES ('Cheddar King');
